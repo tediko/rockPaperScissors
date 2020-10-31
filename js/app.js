84a086b8aa0baa3ea1e1d514febce8a91fefbee9
tediko <tediko123@wp.pl>
2020-10-31 11:11:55 +0100

    Added function to get random computer choice

--- js/app.js
+++ js/app.js
@@ -5,6 +5,11 @@ const result = document.querySelector('[data-result]');
 let userScore = 0;
 let computerScore = 0;
 
+const getComputerChoice = () => {
+    const choices = ['rock', 'paper', 'scissors'];
+    const cpuChoice = Math.floor(Math.random() * choices.length);
+    return choices[cpuChoice];
+}
 
 userChoices.forEach(choice => {
     choice.addEventListener('click', game);
