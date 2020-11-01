c669289dd23f78febdd1feac18db2eeb73952c7a
tediko <tediko123@wp.pl>
2020-11-01 10:57:17 +0100

    Added comments and improve readability

--- js/app.js
+++ js/app.js
@@ -8,86 +8,76 @@ const hands = document.querySelectorAll('.game__result-hands > img')
 let userScore = 0;
 let computerScore = 0;
 
+// Random computer choice
 const getComputerChoice = () => {
     const choices = ['rock', 'paper', 'scissors'];
     const cpuChoice = Math.floor(Math.random() * choices.length);
     return choices[cpuChoice];
 }
 
-const win = (userChoice, cpuChoice) => {
+// Animate hands
+const handDrawAnimation = () => {
     playerHand.style.animation = `handDraw 1500ms`;
     cpuHand.style.animation = `handDrawRotate 1500ms`;
+}
+
+const win = (userChoice, cpuChoice) => {
+    handDrawAnimation();
     setTimeout(() => {
         userScore++;
         playerScore.textContent = userScore;
         result.textContent = `${userChoice} smashes ${cpuChoice}. Player win!`;
-
+        // Update images
         playerHand.src = `assets/img/result-${userChoice}.png`;
         cpuHand.src = `assets/img/result-${cpuChoice}.png`;
         playerScore.classList.add('active');
-    }, 1000)
-    
-
-    // result text animation
-    /* result.classList.remove('lose');
-    result.classList.add('win');
-    setTimeout(() => {
-        playerScore.classList.remove('active');
-    }, 150) */
+        result.classList.add('win');
+    }, 1000)    
 }
-const lose = (userChoice, cpuChoice) => {
-    playerHand.style.animation = `handDraw 1500ms`;
-    cpuHand.style.animation = `handDrawRotate 1500ms`;
 
+const lose = (userChoice, cpuChoice) => {
+    handDrawAnimation();
     setTimeout(() => {
         computerScore++;
         cpuScore.textContent = computerScore;
         result.textContent = `${cpuChoice} smashes ${userChoice}. Cpu win!`;
-
+        // Update images
         playerHand.src = `assets/img/result-${userChoice}.png`;
         cpuHand.src = `assets/img/result-${cpuChoice}.png`;
         cpuScore.classList.add('active');
+        result.classList.add('lose');
     }, 1000)
-
-
-    // result text animation
-    /* result.classList.remove('win');
-    result.classList.add('lose');
-    setTimeout(() => {
-        cpuScore.classList.remove('active');
-    }, 150) */
 }
+
 const tie = (userChoice, cpuChoice) => {
-    playerHand.style.animation = `handDraw 1500ms`;
-    cpuHand.style.animation = `handDrawRotate 1500ms`;
-    
+    handDrawAnimation();    
     setTimeout(() => {
         result.textContent = `It's a tie!`;
-
+        // Update images
         playerHand.src = `assets/img/result-${userChoice}.png`;
         cpuHand.src = `assets/img/result-${cpuChoice}.png`;
     }, 1000)
-
-    // result text animation
-    /* result.classList.add('active');
-    setTimeout(() => {
-        result.classList.remove('active');
-    }, 150) */
 }
 
-function game() {
+function game(event) {
+    const eventTarget = event.target.parentElement;
+    eventTarget.pointerEvents = 'none'; // Prevent clicking on userChoice buttons
     const userChoice = this.dataset.choice;
     const cpuChoice = getComputerChoice();
+
+    // Disable all animations when handDraw animation ends
     hands.forEach(hand => {
         hand.addEventListener('animationend', () => {
             hand.style.animation = '';
+            e.target.parentElement.style.pointerEvents = 'auto'; // Enable clicking on userChoice buttons after animation ends
+            result.classList.remove('win');
+            result.classList.remove('lose');
+            playerScore.classList.remove('active');
+            cpuScore.classList.remove('active');
         })
     })
 
-    // result text animation
-    /* result.classList.remove('lose');
-    result.classList.remove('win'); */
-
+    // Game logic
     switch(userChoice + cpuChoice) {
         case 'rockscissors':
         case 'paperrock':
